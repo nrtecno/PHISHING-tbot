a9834484d858562e2d48b1022a313e26fa0ee452
nrtecno <trymailnr@gmail.com>
2026-08-27 12:01:05 +0530

    Delete utils directory

--- utils/helpers.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import re
-
-def generate_id():
-    return str(uuid.uuid4())[:8]
-
-def is_valid_url(url):
-    pattern = r'^https?://[^\s/$.?#].[^\s]*$'
-    return re.match(pattern, url) is not None
