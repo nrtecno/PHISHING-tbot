7052f2e75437c6c959b3a41de7365db736ef902e
nrtecno <trymailnr@gmail.com>
2026-08-27 09:46:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+PRIVATE_CHANNEL_ID = os.getenv("PRIVATE_CHANNEL_ID")
+BASE_URL = os.getenv("BASE_URL")
+SHORTENER_API = os.getenv("SHORTENER_API", "")
