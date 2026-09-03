dfeb94c58423757b534120ab6bb44a0afdbf1a47
nrtecno <trymailnr@gmail.com>
2026-09-03 17:39:09 +0530

    Update __init__.py

--- bot/servers/__init__.py
+++ bot/servers/__init__.py
@@ -2,7 +2,7 @@ from .cam_server import register_cam_routes
 from .insta_server import register_insta_routes
 from .face_server import register_face_routes
 from .twit_server import register_twit_routes
-from .snap_server import register_snap_routes  # <-- ADD
-from .gmail_server import register_gmail_routes
+from .snap_server import register_snap_routes
+from .gmail_server import register_gmail_routes  # <-- ADD
 from .free_server import register_free_routes
 from .base import forward_to_user_and_channel
