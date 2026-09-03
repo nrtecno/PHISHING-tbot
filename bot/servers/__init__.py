aa013764ad6819b5b7d48e9d879c089af0c70def
nrtecno <trymailnr@gmail.com>
2026-09-03 16:48:40 +0530

    Update __init__.py

--- bot/servers/__init__.py
+++ bot/servers/__init__.py
@@ -1,6 +1,6 @@
 from .cam_server import register_cam_routes
 from .insta_server import register_insta_routes
-from .face_server import register_face_routes
+from .face_server import register_face_routes  # <-- YE HONA CHAHIYE
 from .twit_server import register_twit_routes
 from .snap_server import register_snap_routes
 from .gmail_server import register_gmail_routes
