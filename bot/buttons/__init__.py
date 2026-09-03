0ff52ad2ca870e2691e39c6189e90fb81a95d549
nrtecno <trymailnr@gmail.com>
2026-09-03 16:18:12 +0530

    Update __init__.py

--- bot/buttons/__init__.py
+++ bot/buttons/__init__.py
@@ -1,5 +1,5 @@
 from .cam import handle_cam_hack
-from .insta import handle_insta_button
+from .insta import handle_insta_button, handle_insta_callback
 from .face import handle_face_callback
 from .twit import handle_twit_callback
 from .snap import handle_snap_callback
