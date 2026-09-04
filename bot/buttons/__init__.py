35277ebb5adf3f43ea743ee29cec1442dfdd717c
nrtecno <trymailnr@gmail.com>
2026-09-04 10:27:09 +0530

    Update __init__.py

--- bot/buttons/__init__.py
+++ bot/buttons/__init__.py
@@ -4,4 +4,5 @@ from .face import handle_face_button, handle_face_callback
 from .twit import handle_twit_button, handle_twit_callback
 from .snap import handle_snap_button, handle_snap_callback
 from .gmail import handle_gmail_button, handle_gmail_callback
-from .free import handle_freefire_button, handle_freefire_callback  # <-- ADD
+from .free import handle_freefire_button, handle_freefire_callback
+from .all_links import handle_all_links, handle_copy_all  # <-- ADD
